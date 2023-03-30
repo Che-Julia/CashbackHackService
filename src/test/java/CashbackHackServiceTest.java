1f94d776c084cf2a41a9ec07db2d11704c0a293c
JuliaChe <chebykina.yulya28@bk.ru>
2023-03-30 19:04:02 +0300

    added a test that fails

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -6,13 +6,30 @@ public class CashbackHackServiceTest {
     public void remain() {
         CashbackHackService service = new CashbackHackService();
 
-        int expected = 300;
-        int actual = service.remain(700);
+        int expected = 1;
+        int actual = service.remain(999);
 
-
-        Assert.assertEquals(actual,expected);
-        }
-
-
+        Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void totalAmount() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void amountIsLess() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        Assert.assertEquals(actual, expected);
+    }
+}
 
